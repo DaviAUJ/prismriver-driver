cfdaf4f7de0d2ff1f2f93d1609d76e717409a0d9
daviAUJ <daviaraujon@gmail.com>
2025-08-12 18:30:33 -0300

    Conexão sendo reconhecida pelo driver

--- testes/PrintingDevice.c
+++ testes/PrintingDevice.c
@@ -1,7 +1,52 @@
+#include "linux/printk.h"
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/>
+#include <linux/usb.h>
+#include <linux/hid.h>
+#include <linux/kernel.h>
 
 MODULE_LICENSE("GPL");
-MODULE_LICENSE("Davi");
-MODULE_LICENSE("Printando nome do dispositivo");
+MODULE_AUTHOR("Davi");
+MODULE_DESCRIPTION("Printando conexão e desconexão");
+
+#define GUITAR_ID_VENDOR  0x12BA
+#define GUITAR_ID_PRODUCT 0x0100
+
+static struct hid_device_id table[] = {
+    { HID_USB_DEVICE(GUITAR_ID_VENDOR, GUITAR_ID_PRODUCT), .driver_data = 1 << 14 },
+    {  }
+};
+
+MODULE_DEVICE_TABLE(hid, table);
+
+static int guitarConnect(struct hid_device *interface, const struct hid_device_id *id) {
+    printk(KERN_INFO "Guitarra conectada\n");
+
+    return 0;
+}
+
+static void guitarDisconnect(struct hid_device *inferface) {
+    printk(KERN_INFO "Guitarra desconectada\n");
+}
+
+static struct hid_driver guitar = {
+    .name = "Guitarra PS3",
+    .id_table = table,
+    .probe = guitarConnect,
+    .remove = guitarDisconnect
+};
+
+static int __init guitarInit(void) {
+    printk(KERN_INFO "Modulo inicializado");
+
+    return hid_register_driver(&guitar);
+}
+
+static void __exit guitarExit(void) { 
+    printk(KERN_INFO "Modulo terminado");
+
+    hid_unregister_driver(&guitar);
+}
+
+module_init(guitarInit);
+module_exit(guitarExit);
